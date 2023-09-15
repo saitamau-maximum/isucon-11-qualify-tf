d7f465fea2214e440943dce09b770719974ad96d
sor4chi <sorakawa0218@gmail.com>
2023-09-15 21:42:49 +0900

    feat: add multiple team environment system

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,15 @@ variable "public_key_path" {
 variable "zone" {
   default = "tk1b"
 }
+
+variable "app_name" {
+  default = "isucon11q-app"
+}
+
+variable "switch_name" {
+  default = "isucon11q-switch"
+}
+
+variable "benchmarker_name" {
+  default = "isucon11q-benchmarker"
+}
